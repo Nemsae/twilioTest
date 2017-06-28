500d960ff74234cf4d2b943e88dcea0fe0341f0f
John Son <jsson77@gmail.com>
2017-06-28 16:41:28 -0700

    move keys out to .env

--- server/routes/twilio.js
+++ server/routes/twilio.js
@@ -1,28 +1,27 @@
-const accountSid = 'AC3873ec885fb8d993355e77956fb0c930'; // Your Account SID from www.twilio.com/console
-const authToken = '9244de510c4ddad9e4d9e46240768791';   // Your Auth Token from www.twilio.com/console
+require('dotenv').config({ silent: true });
 
 const Twilio = require('twilio');
 const express = require('express');
 
 const router = express.Router();
 
-const client = new Twilio(accountSid, authToken);
+const client = new Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
 router.route('/')
 .post((req, res) => {
   const messageReq = req.body;
   console.log('messageReq: ', messageReq);
   client.messages.create({
-    // body: messageReq.body,
-    // to: messageReq.receiver,  // Text this number
-    // from: messageReq.sender, // From a valid Twilio number
-    body: 'Hardcoded',
-    to: '+16506782956',  // Text this number
-    from: '+16504259920', // From a valid Twilio number
+    body: messageReq.body,
+    to: messageReq.receiver,  // Text this number
+    from: messageReq.sender, // From a valid Twilio number
+    // body: 'Hardcoded',
+    // to: '+16506782956',  // Text this number
+    // from: '+16504259920', // From a valid Twilio number
   })
   .then((message) => {
     console.log('message: ', message);
-    console.log(message.sid);
+    // console.log(message.sid);
     res.send(message.sid);
   })
   .catch((err) => {
