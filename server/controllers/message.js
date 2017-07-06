8364374fd3c66fceca346aaa36efa98b8b6a4a77
John Son <jsson77@gmail.com>
2017-07-06 12:57:04 -0700

    move controller logic for sending simple message from Twilio to receiver to controllers folder

--- server/controllers/message.js
+++ server/controllers/message.js
@@ -1,8 +1,11 @@
+require('dotenv').config({ silent: true });
+const Twilio = require('twilio');
 const Subscriber = require('../models/Subscriber');
 const messageSender = require('../lib/messageSender');
 
-//  Creating a function to handle Twilio SMS/MMS webhook requests
+const client = new Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
+//  Creating a function to handle Twilio SMS/MMS webhook requests
 exports.webhook = (request, response) => {
   //  Grabbing user's phone number
   const phone = request.body.From;
@@ -71,9 +74,8 @@ exports.webhook = (request, response) => {
   }
 };
 
-//  Handle form submission
+//  Sending a message to subscriber(S) from client
 exports.sendMessages = (request, response) => {
-  //  Get message info from form submission
   const message = `${request.body.sender} says "${request.body.message}"`;
   const imageUrl = request.body.imageUrl;
 
@@ -87,7 +89,24 @@ exports.sendMessages = (request, response) => {
   }).catch((err) => {
     console.log(`err ${err.meessage}`);  // eslint-disable-line no-console
     response.status(400).send(err.message);
-    // request.flash('errors', err.message);
-    // response.redirect('/');
+  });
+};
+
+//  Sending a simple message
+exports.sendSimpleMessage = (request, response) => {
+  const messageReq = request.body;
+  console.log('messageReq: ', messageReq);  // eslint-disable-line no-console
+
+  client.messages.create({
+    body: messageReq.body,
+    to: messageReq.receiver,  // Text this number ex. "+16506782956"
+    from: messageReq.sender, // From a valid Twilio number ex. "+16504259920"
+  })
+  .then((message) => {
+    response.send(message.sid);
+  })
+  .catch((err) => {
+    console.log('ERROR:POST:/api/twilio ', err);  // eslint-disable-line no-console
+    response.status(400).send(err);
   });
 };
--- server/routes/twilio.js
+++ server/routes/twilio.js
@@ -1,36 +1,11 @@
-require('dotenv').config({ silent: true });
-const Twilio = require('twilio');
 const express = require('express');
 const message = require('../controllers/message');
 
 const router = express.Router();
-const client = new Twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
 
 router.route('/')
 //  Sending a message to specified receiver from our Twilio account
-.post((req, res) => {
-  const messageReq = req.body;
-  console.log('messageReq: ', messageReq);  // eslint-disable-line no-console
-  client.messages.create({
-    body: messageReq.body,
-    to: messageReq.receiver,  // Text this number ex. "+16506782956"
-    from: messageReq.sender, // From a valid Twilio number ex. "+16504259920"
-  })
-  .then((message) => {
-    res.send(message.sid);
-  })
-  .catch((err) => {
-    console.log('ERROR:POST:/api/twilio ', err);  // eslint-disable-line no-console
-    res.status(400).send(err);
-  });
-});
-
-router.route('/inbound')
-//  When someone texts our Twilio account, we respond with a message
-.get((req, res) => {
-  res.set('Content-Type', 'text/xml');
-  res.send('<Response><Message>Rixi says hi!</Message></Response>');
-});
+.post(message.sendSimpleMessage);
 
 router.route('/message/send')
 //  Handle form submission and send messages to subscribers
@@ -39,11 +14,12 @@ router.route('/message/send')
 router.route('/message')
 //  When a user texts our number
 .post(message.webhook);
-// .post('/message', message.webhook);
 
-
-//  Render a page that will allow an admin to send out a message to subs
-// app.get('/', pages.showForm);
-
+router.route('/inbound')
+//  When someone texts our Twilio account, we respond with a message
+.get((req, res) => {
+  res.set('Content-Type', 'text/xml');
+  res.send('<Response><Message>Rixi says hi!</Message></Response>');
+});
 
 module.exports = router;
